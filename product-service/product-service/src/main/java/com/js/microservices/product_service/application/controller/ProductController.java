c74625ce42036a93b24efce3d8c4cb85cbc71bf7
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-12-22 19:01:24 -0300

    [ :video_game: ] | create: controller - product (module::application)

---
+++ product-service/product-service/src/main/java/com/js/microservices/product_service/application/controller/ProductController.java
@@ -0,0 +1,34 @@
+package com.js.microservices.product_service.application.controller;
+
+import com.js.microservices.product_service.domain.dto.CreateProductDTO;
+import com.js.microservices.product_service.domain.dto.ProductCreatedDTO;
+import com.js.microservices.product_service.domain.service.ProductService;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.js.microservices.product_service.application.factory.product.ProductFactory.toCreatedDTO;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService service;
+
+    @PostMapping
+    @ResponseStatus(CREATED)
+    public ProductCreatedDTO createProduct(CreateProductDTO dto) {
+        return toCreatedDTO(service.create(dto));
+    }
+
+    @GetMapping
+    @ResponseStatus(OK)
+    public List<ProductCreatedDTO> getAllProducts() {
+        return toCreatedDTO(service.getAll());
+    }
+}
