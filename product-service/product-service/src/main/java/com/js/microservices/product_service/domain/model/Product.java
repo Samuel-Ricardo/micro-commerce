4e24c49817f9a62d8c31ebb3c93323ae282f4b7c
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-12-22 18:30:42 -0300

    [ :sparkles: ] | create: model - product (module::domain)

---
+++ product-service/product-service/src/main/java/com/js/microservices/product_service/domain/model/Product.java
@@ -0,0 +1,17 @@
+package com.js.microservices.product_service.domain.model;
+
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+
+@Document (value = "products")
+@Data @Builder
+@AllArgsConstructor @NoArgsConstructor
+public class Product {
+    private String id;
+    private String name;
+    private String description;
+    private String skuCode;
+    private BigDecimal price;
+}
