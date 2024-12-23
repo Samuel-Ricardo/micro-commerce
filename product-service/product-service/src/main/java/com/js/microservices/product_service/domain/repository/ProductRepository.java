2d70934989a5a822601c6292aa131cd54f69afa1
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-12-23 15:49:19 -0300

    [ :recycle: ] | rename: model - Product -> ProductModel (module:domain)

--- product-service/product-service/src/main/java/com/js/microservices/product_service/domain/repository/ProductRepository.java
+++ product-service/product-service/src/main/java/com/js/microservices/product_service/domain/repository/ProductRepository.java
@@ -1,6 +1,6 @@
 package com.js.microservices.product_service.domain.repository;
 
-import com.js.microservices.product_service.domain.model.Product;
+import com.js.microservices.product_service.domain.model.ProductModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ProductRepository extends MongoRepository<Product, String> {}
+public interface ProductRepository extends MongoRepository<ProductModel, String> {}
---
+++ product-service/product-service/src/main/java/com/js/microservices/product_service/domain/repository/ProductRepository.java~
@@ -0,0 +1,6 @@
+package com.js.microservices.product_service.domain.repository;
+
+import com.js.microservices.product_service.domain.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {}
