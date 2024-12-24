ae4f0698b63770d7c80e827648764c6a3d4a3e15
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-12-24 20:01:09 -0300

    [ :sparkles: ] | create: value object - order > price [total] (module::domain)

--- order-service/src/main/java/com/js/microservices/order_service/domain/vo/Price.kt
+++ order-service/src/main/java/com/js/microservices/order_service/domain/vo/Price.kt
@@ -4,4 +4,11 @@ import java.math.BigDecimal
 
 data class Price(
     val value: BigDecimal
-)
+) {
+    companion object {
+        @JvmStatic
+        fun totalPrice(price: BigDecimal, quantity: Int): Price {
+            return Price(price.multiply(BigDecimal(quantity)))
+        }
+    }
+}
