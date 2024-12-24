05e577b419bfa27cff895b804a4d84e4047d8535
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-12-24 14:27:02 -0300

    [ :sparkles: ] | create: DTO - place > order (module::domain)

---
+++ order-service/src/main/java/com/js/microservices/order_service/domain/dto/PlaceOrderDTO.kt
@@ -0,0 +1,18 @@
+package com.js.microservices.order_service.domain.dto
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+import java.math.BigDecimal
+
+data class PlaceOrderDTO(
+    @NotBlank
+    val orderNumber: String,
+    @NotBlank
+    val skuCode: String,
+    @NotNull
+    val price: BigDecimal,
+    @NotNull
+    val quantity: Int,
+    @NotNull
+    val userDetails: UserDetailsDTO
+)
