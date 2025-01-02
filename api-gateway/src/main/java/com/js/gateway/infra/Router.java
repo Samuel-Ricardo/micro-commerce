e2af6a9f9abc30817c51de2f413057b2d160d4da
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-01-02 11:17:44 -0300

    [ :vertical_traffic_light: ] | setup: router - docs (app::router)

--- api-gateway/src/main/java/com/js/gateway/infra/Router.java
+++ api-gateway/src/main/java/com/js/gateway/infra/Router.java
@@ -3,12 +3,16 @@ package com.js.gateway.infra;
 import org.springframework.beans.factory.annotation.Value;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
 
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
+
 
 @Configuration
 public class Router {
@@ -30,9 +34,25 @@ public class Router {
     }
 
     @Bean
+    public RouterFunction<ServerResponse> productServiceSwagger() {
+        return GatewayRouterFunctions.route("product_service_swagger")
+                .route(RequestPredicates.path("/aggregate/product-service/v3/api-docs"), http(productServiceUrl))
+                .filter(setPath("/api-docs"))
+                .build();
+    }
+
+    @Bean
     public RouterFunction<ServerResponse> orderService() {
         return GatewayRouterFunctions.route("order_service")
                 .route(RequestPredicates.path("/api/v1/orders/**"), http(orderServiceUrl))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceSwagger() {
+        return GatewayRouterFunctions.route("order_service_swagger")
+                .route(RequestPredicates.path("/aggregate/order-service/v3/api-docs"), http(orderServiceUrl))
+                .filter(setPath("/api-docs"))
                 .build();
     }
 
@@ -42,4 +62,13 @@ public class Router {
                 .route(RequestPredicates.path("/api/v1/inventory/**"), http(inventoryServiceUrl))
                 .build();
     }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceSwagger() {
+        return GatewayRouterFunctions.route("inventory_service_swagger")
+                .route(RequestPredicates.path("/aggregate/inventory-service/v3/api-docs"), http(inventoryServiceUrl))
+                .filter(setPath("/api-docs"))
+                .build();
+    }
 }
+
