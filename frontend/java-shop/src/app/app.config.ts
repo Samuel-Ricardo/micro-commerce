83529f1f6ae959df2b69e4b1d898cd4840e47c56
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-01-05 18:19:32 -0300

    [ :gear: ] | create: config - auth (app::config)

--- frontend/java-shop/src/app/app.config.ts
+++ frontend/java-shop/src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { authConfig } from './auth/auth.config';
+import { provideAuth } from 'angular-auth-oidc-client';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideAuth(authConfig)]
 };
