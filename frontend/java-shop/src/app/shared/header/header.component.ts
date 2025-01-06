780bab2a075b3e4151962aa18704797fa8cc6ce6
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-01-06 02:50:07 -0300

    [ :art: ] | setup: component - header (frontend::shared)

--- frontend/java-shop/src/app/shared/header/header.component.ts
+++ frontend/java-shop/src/app/shared/header/header.component.ts
@@ -8,4 +8,18 @@ import { OidcSecurityService } from 'angular-auth-oidc-client';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent implements OnInit {}
+export class HeaderComponent implements OnInit {
+  private readonly securityService = inject(OidcSecurityService);
+  isAuthenticated: boolean = false;
+  username: string = '';
+
+  ngOnInit(): void {
+    this.securityService.isAuthenticated$.subscribe(({ isAuthenticated }) => {
+      this.isAuthenticated = isAuthenticated;
+    });
+
+    this.securityService.userData$.subscribe(({ userData }) => {
+      if (userData) this.username = userData['preferred_username']; // userData.preferred_username;
+    });
+  }
+}
