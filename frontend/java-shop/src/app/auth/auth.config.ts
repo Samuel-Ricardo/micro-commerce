d79cc3b0e96f268e572b5699cf31656d76c41f39
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-01-06 02:12:49 -0300

    [ :gear: ] | update: config - auth (app::config)

--- frontend/java-shop/src/app/auth/auth.config.ts
+++ frontend/java-shop/src/app/auth/auth.config.ts
@@ -2,14 +2,15 @@ import { PassedInitialConfig } from 'angular-auth-oidc-client';
 
 export const authConfig: PassedInitialConfig = {
   config: {
-              authority: '',
-              redirectUrl: window.location.origin,
-              postLogoutRedirectUri: window.location.origin,
-              clientId: 'please-enter-clientId',
-              scope: 'please-enter-scopes', // 'openid profile offline_access ' + your scopes
-              responseType: 'code',
-              silentRenew: true,
-              useRefreshToken: true,
-              renewTimeBeforeTokenExpiresInSeconds: 30,
-          }
-}
+    authority:
+      'http://keycloak:8181/realms/java-shop-microservices-security-realm',
+    redirectUrl: window.location.origin,
+    postLogoutRedirectUri: window.location.origin,
+    clientId: 'frontend-client',
+    scope: 'openid profile offline_access',
+    responseType: 'code',
+    silentRenew: true,
+    useRefreshToken: true,
+    renewTimeBeforeTokenExpiresInSeconds: 30,
+  },
+};
