5e266c03e4e3a7f7a5a0071d782f4427598c0e09
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-01-06 04:41:27 -0300

    [ :white_check_mark: ] | test: page - app-root (app::test)

--- frontend/java-shop/src/app/app.component.spec.ts
+++ frontend/java-shop/src/app/app.component.spec.ts
@@ -24,6 +24,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, java-shop');
+    expect(compiled.querySelector('span')?.textContent).toContain(
+      'Spring Boot Microsservices Java Shop :D',
+    );
   });
 });
