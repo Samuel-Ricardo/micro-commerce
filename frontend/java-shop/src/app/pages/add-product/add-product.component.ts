79f1c0ebc4513fc476dab366788f9a22321fbbc7
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-01-06 04:19:14 -0300

    [ :page_with_curl: ] | setup: page - add-product (app::page)

--- frontend/java-shop/src/app/pages/add-product/add-product.component.ts
+++ frontend/java-shop/src/app/pages/add-product/add-product.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ProductServiceService } from '../../application/service/product-service.service';
 
 @Component({
   selector: 'app-add-product',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './add-product.component.html',
-  styleUrl: './add-product.component.scss'
+  styleUrl: './add-product.component.scss',
 })
 export class AddProductComponent {
+  private readonly productService = inject(ProductServiceService);
 
+  addProduct: FormGroup;
+  productCreated: boolean = false;
+
+  constructor(private fb: FormBuilder) {
+    this.addProduct = this.fb.group({
+      skuCode: ['', [Validators.required]],
+      name: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+      price: [0, [Validators.required]],
+    });
+  }
 }
