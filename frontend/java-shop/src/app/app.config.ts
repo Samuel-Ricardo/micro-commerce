31b6147fd4c4ed1e397e43d69e3fbe5e4b892245
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-01-06 04:44:10 -0300

    [ :gear: ] | update: config - app (app::config)

--- frontend/java-shop/src/app/app.config.ts
+++ frontend/java-shop/src/app/app.config.ts
@@ -1,10 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { authConfig } from './auth/auth.config';
 import { provideAuth } from 'angular-auth-oidc-client';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './interceptor/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAuth(authConfig)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideAuth(authConfig),
+  ],
 };
