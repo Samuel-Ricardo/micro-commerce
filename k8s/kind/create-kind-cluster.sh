dd48bfda96fbe40d47b9bd2457643ed39dfdb2c0
samuel_ricardo <samuelricardoofficial@gmail.com>
2025-01-17 06:46:05 -0300

    [ :gear: ] | update: script - create-kind-cluster.sh (k8s::config)

--- k8s/kind/create-kind-cluster.sh
+++ k8s/kind/create-kind-cluster.sh
@@ -8,3 +8,12 @@ kind create cluster --name java-shop --config kind-config.yaml
 #./kind-load.sh
 
 echo "===Kind Cluster Started=== 🎉"
+
+echo ""
+
+echo "===Starting Contaiers=== 🚀"
+
+chmod +x ./apply_all.sh
+./apply_all.sh
+
+echo "===Contaiers Started=== 🎉"
